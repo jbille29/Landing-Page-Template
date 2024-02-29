ed4516042f136795014e9d91de2e1eeaa6af3f54
jbille29 <jbille29@gmail.com>
2024-02-28 19:14:11 -0600

    added layout for mobile

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import cameraImg from './assets/camera.png'
+import heroImg from './assets/hero.png'
+
+const App = () => {
+ 
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="landing-container">
+
+      <div className="copy-container">
+
+        <h1>Whoa! A header</h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quisquam odio quod omnis, adipisci velit recusandae itaque consequuntur inventore ullam dolore.</p>
+        <div className="signup-container">
+          <label htmlFor=""></label>
+          <input type="email" />
+          <button>Sign up</button>
+        </div>
+        
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
+      <div className="hero-container">
+        
+        <img className='hero' src={heroImg} alt="" />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+    </div>
   )
 }
 
