3f049b8cc6b3080d3a58361920acd94e65c92506
jbille29 <jbille29@gmail.com>
2024-03-03 09:51:49 -0600

    styles for mobile

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,31 @@
 
 import cameraImg from './assets/camera.png'
-import heroImg from './assets/hero.png'
+import heroImg from './assets/cat gif.gif'
+
+import Navbar from './components/Navbar'
 
 const App = () => {
  
 
   return (
     <div className="landing-container">
+      <Navbar/>
 
       <div className="copy-container">
 
-        <h1>Whoa! A header</h1>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quisquam odio quod omnis, adipisci velit recusandae itaque consequuntur inventore ullam dolore.</p>
+        <h1>Light up your cat's world with Purrjection</h1>
+        <p>Millions of companies of all sizes use Stripe online and in person to accept payments, send payouts, automate financial processes, and ultimately grow revenue.</p>
         <div className="signup-container">
           <label htmlFor=""></label>
-          <input type="email" />
+          <input type="email" 
+            placeholder='cool-cat-owner@catsrule.com'/>
           <button>Sign up</button>
         </div>
         
       </div>
 
       <div className="hero-container">
-        
+     
         <img className='hero' src={heroImg} alt="" />
       </div>
 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import logo from '../assets/purrjection.png'
+
+const Navbar = () => {
+  return (
+    <nav>
+        <img src={logo} alt="" />
+    </nav>
+  )
+}
+
+export default Navbar
